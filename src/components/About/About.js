14f7a73c3f71d9f758bbcb9185f787eba9683f28
Andrey <ScootalooRainbow@yandex.ru>
2020-12-27 20:58:03 +0900

    about ready

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,11 +5,13 @@ import "./about/about.css";
 function About() {
   return (
     <div className="about">
+      <div className="about__content">
     <img className="about__avatar" src='https://s15.stc.all.kpcdn.net/russia/wp-content/uploads/2020/05/devushka-v-shlyape.jpg' />
      <div className="about__text">
       <h2 className="about__title">Об авторе</h2>
       <p className="about__subtitle">Это блок с описанием автора проекта. Здесь следует указать, как вас зовут, чем вы занимаетесь, какими технологиями разработки владеете.</p>
       <p className="about__subtitle">Также можно рассказать о процессе обучения в Практикуме, чему вы тут научились, и чем можете помочь потенциальным заказчикам.</p>
+    </div>
     </div>
    </div>
   )
