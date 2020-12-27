cf7812329cfd777e33cfc782ae8f2ea048d56a02
Andrey <ScootalooRainbow@yandex.ru>
2020-12-28 02:08:23 +0900

    footer v1

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
-import {Link, NavLink, Route } from 'react-router-dom';
 import facebook from "../../images/facebook.svg"
 import github from "../../images/github.svg"
+import { Link } from 'react-router-dom';
 import "./footer/footer.css";
 
 function Footer() {
@@ -9,13 +9,15 @@ function Footer() {
     <footer className="footer">
       <p className="footer__copyright">© 2020 Supersite, Powered by News API</p>
       <nav className="footer__nav">
-        <div className="links">
-        <Link className="link link_theme_black" exact to="/">Яндекс.Практикум</Link>
-        <Link className="link link_theme_black" exact to="/">Главная</Link>
+        <div className="footer__links">
+        <a className="link link_theme_black" href="/">Главная</a>
+        <a className="link link_theme_black" target="_blank" href='https://praktikum.yandex.ru'>Яндекс.Практикум</a>
         </div>
         <div className="footer__icons">
-        <img className="footer__social" src={facebook} />
+        <a target="_blank" href='https://facebook.com'> <img className="footer__social" src={facebook} /> </a>
+        <a target="_blank" href="https://github.com/Cabagemage">
         <img className="footer__social" src={github} />
+        </a>
         </div>
       </nav>
    </footer>
