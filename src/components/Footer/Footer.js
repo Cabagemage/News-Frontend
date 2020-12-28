4477c4dbd63ed0c6d19ea0be5b2bdaf9d17ec57b
Andrey <ScootalooRainbow@yandex.ru>
2020-12-28 10:28:14 +0900

    Fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,36 +8,34 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__copyright">© 2020 Supersite, Powered by News API</p>
-
       <div className="footer__menu">
-      <nav className="footer__nav">
-        <ul className="footer__links">
-          <li className="footer__list-item">
-            <a className="link link_theme_black footer__link" href="/">
-              Главная
-            </a>
-          </li>
-          <li className="footer__list-item">
-            <a
-              className="link link_theme_black footer__link"
-              target="_blank"
-              href="https://praktikum.yandex.ru"
-            >
-              Яндекс.Практикум
-            </a>
-          </li>
-        </ul>
+        <nav className="footer__nav">
+          <ul className="footer__links">
+            <li className="footer__list-item">
+              <a className="link link_theme_black footer__link" href="/">
+                Главная
+              </a>
+            </li>
+            <li className="footer__list-item">
+              <a
+                className="link link_theme_black footer__link"
+                target="_blank"
+                href="https://praktikum.yandex.ru"
+              >
+                Яндекс.Практикум
+              </a>
+            </li>
+          </ul>
         </nav>
         <div className="footer__icons">
           <a target="_blank" href="https://facebook.com">
-            {" "}
-            <img className="footer__social" src={facebook} />{" "}
+            <img className="footer__social" src={facebook} />
           </a>
           <a target="_blank" href="https://github.com/Cabagemage">
             <img className="footer__social" src={github} />
           </a>
         </div>
-        </div>
+      </div>
     </footer>
   );
 }
