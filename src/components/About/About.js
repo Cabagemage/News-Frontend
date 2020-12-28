390f5e23f1448c151276731e2a25eb0165a747dd
Andrey <ScootalooRainbow@yandex.ru>
2020-12-28 23:32:20 +0900

    fixed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import "./about/about.css";
 
@@ -6,15 +6,24 @@ function About() {
   return (
     <div className="about">
       <div className="about__content">
-    <img className="about__avatar" src='https://s15.stc.all.kpcdn.net/russia/wp-content/uploads/2020/05/devushka-v-shlyape.jpg' />
-     <div className="about__text">
-      <h2 className="about__title">Об авторе</h2>
-      <p className="about__subtitle">Это блок с описанием автора проекта. Здесь следует указать, как вас зовут, чем вы занимаетесь, какими технологиями разработки владеете.</p>
-      <p className="about__subtitle">Также можно рассказать о процессе обучения в Практикуме, чему вы тут научились, и чем можете помочь потенциальным заказчикам.</p>
+        <img
+          className="about__avatar"
+          src="https://s15.stc.all.kpcdn.net/russia/wp-content/uploads/2020/05/devushka-v-shlyape.jpg"
+        />
+        <div className="about__text">
+          <h2 className="about__title">Об авторе</h2>
+          <p className="about__subtitle">
+            Это блок с описанием автора проекта. Здесь следует указать, как вас
+            зовут, чем вы занимаетесь, какими технологиями разработки владеете.
+          </p>
+          <p className="about__subtitle">
+            Также можно рассказать о процессе обучения в Практикуме, чему вы тут
+            научились, и чем можете помочь потенциальным заказчикам.
+          </p>
+        </div>
+      </div>
     </div>
-    </div>
-   </div>
-  )
+  );
 }
 
 export default About;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,6 +5,10 @@ import "./card/card.css";
 function Card({ loggedIn }) {
   const [isShown, setIsShown] = useState(false);
 
+  const phrase =
+    "В 2016 году Америка отмечала важный юбилей: сто лет назад здесь начала складываться система национальных парков";
+  const phraseSub = phrase.substring(0, 130) + "...";
+
   return (
     <div className="card">
       <img className="card__image" src={testImage}></img>
@@ -33,7 +37,8 @@ function Card({ loggedIn }) {
       <div className="card__text">
         <p className="card__date">12 августа, 2019</p>
         <h2 className="card__article">OneTwoThree</h2>
-        <p className="card__about">История этой карточки очень любопытная</p>
+        <p className="card__about"> {phraseSub}</p>
+
         <a href="#" className="card__source">
           Медуза
         </a>
