f1a3f5cde91d4035ac7423ae25754edff97f726d
Andrey <ScootalooRainbow@yandex.ru>
2020-12-30 00:39:22 +0900

    First iteration@

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,7 +5,7 @@ import "./main/main.css";
 function Main() {
   return (
     <div className="main">
-      <h2 className="main__title">Что творится в мире?</h2>
+      <h1 className="main__title">Что творится в мире?</h1>
       <h3 className="main__info">
         Находите самые свежие статьи на любую тему и сохраняйте в своем личном
         кабинете
